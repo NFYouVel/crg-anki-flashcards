f4e0a2da19f357d866660df59751dbfe8fbb00ef
NFYouVel <marvelnathanael072006@gmail.com>
2025-06-22 22:23:44 +0700

    ui login selesai

--- Pages/Login/index.php
+++ Pages/Login/index.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="icon" href="../../Logo/circle.png">
+    <link rel="stylesheet" href="CSS/index.css">
 </head>
 <body>
     <?php include "../Global Assets/header.php"; ?>
+    <div class="wrapper">
+        <div class="form-side">
+            <span class="h2">Login</span>
+            <span class="description">Login in to your account</span>
+            <form action="">
+                <input type="email" name="email" placeholder="Email"> <br>
+                <input type="password" name="email" placeholder="Password"> <br>
+                <input type="submit" value="Log In" name="submit" class="submit">
+            </form>
+            <span class="alert">Wrong email or password. Please contact our admin</span>
+        </div>
+    </div>
 </body>
 </html>
