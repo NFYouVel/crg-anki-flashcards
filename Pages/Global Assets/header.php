79ce40fef0ba99be414a45760dd7b253d9a0eb62
NFYouVel <marvelnathanael072006@gmail.com>
2025-07-16 15:36:44 +0700

    update

--- Pages/Global Assets/header.php
+++ Pages/Global Assets/header.php
@@ -1,58 +1,62 @@
 <style>
     * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
-
-html, body {
-    height: 100%;
-}
-
-.wrapper-header {
-    display: flex;
-    z-index: 100;
-    position: relative;
-}
-
-.wrapper-header .header {
-    display: flex;
-    background-color: rgb(255, 165, 5);
-    width: 100%;
-    height: 9vh;
-    align-items: center;
-}
-
-.logo {
-    display: flex;
-    height: 90%;
-    align-items: center;
-    margin: 8px;
-}
-
-.wrapper-header .header .logo img{
-    object-fit: cover;
-    height: 87%;
-
-    filter: 
-  drop-shadow(0 0 1.5px white)
-  drop-shadow(0 0 1.5px white)
-  drop-shadow(0 0 1.5px white)
-  drop-shadow(0 0 1.5px white)
-  drop-shadow(0 0 1.5px white);
-}
-
-@media screen and (max-width: 768px) {
-    .logo {
-        padding: 3px 0;
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
     }
-}
+
+    html,
+    body {
+        height: 100%;
+    }
+
+    .wrapper-header {
+        display: flex;
+        z-index: 100;
+        position: relative;
+    }
+
+    .wrapper-header .header {
+        display: flex;
+        background-color: rgb(255, 165, 5);
+        width: 100%;
+        height: 9vh;
+        align-items: center;
+    }
+
+    .logo {
+        display: flex;
+        height: 90%;
+        align-items: center;
+        margin: 8px;
+    }
+
+    .wrapper-header .header .logo img {
+        object-fit: cover;
+        height: 87%;
+
+        filter:
+            drop-shadow(0 0 1.5px white) drop-shadow(0 0 1.5px white) drop-shadow(0 0 1.5px white) drop-shadow(0 0 1.5px white) drop-shadow(0 0 1.5px white);
+    }
+
+    @media screen and (max-width: 768px) {
+        .logo {
+            padding: 3px 0;
+        }
+    }
 </style>
 <div class="wrapper-header">
     <!-- Untuk Logo di atas (header) -->
     <div class="header">
         <div class="logo">
-            <img src="../../Logo/1080.png" alt="CRG Logo">
+            <img src="../../Logo/1080.png" alt="CRG Logo" onclick="BackHome()" style="cursor:pointer;">
         </div>
     </div>
 </div>
+
+<script>
+    function BackHome() {
+        window.location.href = "index.php"
+    }
+
+</script>
