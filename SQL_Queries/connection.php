00bbf62e3443b70d96ebb77a463fd197435d638a
Leon080306 <leonsamuels080306@gmail.com>
2025-08-10 22:12:36 +0700

    coba hosting

--- SQL_Queries/connection.php
+++ SQL_Queries/connection.php
@@ -1,3 +1,8 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "anki");
+    $con = mysqli_connect("202.10.36.216", "root", "", "anki");
+
+    if (!$con) {
+        echo "Koneksi gagal";
+    }
+    echo "Koneksi berhasil!";
 ?>
