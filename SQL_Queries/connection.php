a523be17a5db02826b8be1c087e5fd5079eae2a7
Leon080306 <leonsamuels080306@gmail.com>
2025-08-25 00:14:35 +0700

    reset session

--- SQL_Queries/connection.php
+++ SQL_Queries/connection.php
@@ -1,9 +1,4 @@
 <?php
     $con = mysqli_connect("localhost", "anki_marvel", "ihatep0tat0", "anki");
-
-    if(!isset($_SESSION["user_id"])) {
-        header("Location: ../Pages/Login");
-        exit;
-    }
     // $con = mysqli_connect("localhost", "root", "", "anki");
 ?>
