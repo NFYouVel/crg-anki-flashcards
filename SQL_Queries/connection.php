e1d6e217a7c57f76cb6dcf7bcd89651da6106a90
Leon080306 <leonsamuels080306@gmail.com>
2025-08-25 00:13:34 +0700

    tes

--- SQL_Queries/connection.php
+++ SQL_Queries/connection.php
@@ -1,7 +1,4 @@
 <?php
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
     $con = mysqli_connect("localhost", "anki_marvel", "ihatep0tat0", "anki");
 
     if(!isset($_SESSION["user_id"])) {
