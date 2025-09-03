65cf404d553df3c812c7a9cb70876b89e04d4b98
Marvel Nathanael <marvelnathanael072006@gmail.com>
2025-09-03 23:49:48 +0700

    update 2 bug

--- Pages/Home/classroom_information.php
+++ Pages/Home/classroom_information.php
@@ -10,7 +10,7 @@ $query = "SELECT * FROM users WHERE user_id = '$user_id'";
 $result = mysqli_query($con, $query);
 $line = mysqli_fetch_array($result);
 $role_id = $line['role'];
-if($role_id != 2) {
+if ($role_id != 2) {
     header("Location: ../Login");
 }
 $result2 = mysqli_query($con, "SELECT * FROM user_role WHERE role_id = '$role_id'");
@@ -55,7 +55,7 @@ $classroom_name = mysqli_fetch_array($result_classroom);
                 xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
 
-            xmlhttp.onreadystatechange = function () {
+            xmlhttp.onreadystatechange = function() {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                     document.querySelector(".explanationWrapper").innerHTML = xmlhttp.responseText;
                 }
@@ -63,6 +63,7 @@ $classroom_name = mysqli_fetch_array($result_classroom);
             xmlhttp.open("GET", "jQuery/ajax_search_deck.php?deckName=" + str + "&classroomID=<?php echo $classroom_id; ?>&teacherID=<?php echo $user_id; ?>", true);
             xmlhttp.send();
         }
+
         function addDeck(str) {
             var xmlhttp;
             if (window.XMLHttpRequest != null) {
@@ -71,7 +72,7 @@ $classroom_name = mysqli_fetch_array($result_classroom);
                 xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
 
-            xmlhttp.onreadystatechange = function () {
+            xmlhttp.onreadystatechange = function() {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                     document.querySelector(".explanationWrapper").innerHTML = xmlhttp.responseText;
                 }
@@ -79,6 +80,7 @@ $classroom_name = mysqli_fetch_array($result_classroom);
             xmlhttp.open("GET", "jQuery/ajax_addDeckClassroom.php?deckID=" + str + "&classroomID=<?php echo $classroom_id; ?>&teacherID=<?php echo $user_id; ?>", true);
             xmlhttp.send();
         }
+
         function removeDeck(str) {
             var xmlhttp;
             if (window.XMLHttpRequest != null) {
@@ -87,7 +89,7 @@ $classroom_name = mysqli_fetch_array($result_classroom);
                 xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
             }
 
-            xmlhttp.onreadystatechange = function () {
+            xmlhttp.onreadystatechange = function() {
                 if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                     document.querySelector(".explanationWrapper").innerHTML = xmlhttp.responseText;
                 }
@@ -95,7 +97,6 @@ $classroom_name = mysqli_fetch_array($result_classroom);
             xmlhttp.open("GET", "jQuery/ajax_removeDeckClassroom.php?deckID=" + str + "&classroomID=<?php echo $classroom_id; ?>&teacherID=<?php echo $user_id; ?>", true);
             xmlhttp.send();
         }
-        
     </script>
 </head>
 
@@ -135,69 +136,69 @@ $classroom_name = mysqli_fetch_array($result_classroom);
                 <span>(If the student already has the deck, then the deck will not be added)</span>
             </div>
             <div class='search-bar'>
-                <input type="text" onkeyup = "searchDeck(this.value)" placeholder="Search deck" name="search" class="search-bar">
+                <input type="text" onkeyup="searchDeck(this.value)" placeholder="Search deck" name="search" class="search-bar">
                 <div class="icon-add">🔍</div>
-            </div><hr>
+            </div>
+            <hr>
             <div class='explanation'>
                 <div class="explanationWrapper">
                     <?php
-                        function getDecks($parentID) {
-                            global $classroom_id;
-                            global $con;
-                            if ($parentID == "root") {
-                                $getDecks = mysqli_query($con, "SELECT deck_id, name, parent_deck_id, is_leaf FROM decks WHERE parent_deck_id IS NULL ORDER BY name ASC");
-                            } else {
-                                $getDecks = mysqli_query($con, "SELECT deck_id, name, parent_deck_id, is_leaf FROM decks WHERE deck_id = '$parentID'");
-                            }
-                            if (mysqli_num_rows($getDecks) > 0) {
-                                while ($deck = mysqli_fetch_assoc($getDecks)) {
-                                    $deckID = $deck["deck_id"];
-                                    $name = $deck["name"];
-                                    $isLeaf = $deck["is_leaf"];
+                    function getDecks($parentID)
+                    {
+                        global $classroom_id;
+                        global $con;
+                        if ($parentID == "root") {
+                            $getDecks = mysqli_query($con, "SELECT deck_id, name, parent_deck_id, is_leaf FROM decks WHERE parent_deck_id IS NULL ORDER BY name ASC");
+                        } else {
+                            $getDecks = mysqli_query($con, "SELECT deck_id, name, parent_deck_id, is_leaf FROM decks WHERE deck_id = '$parentID'");
+                        }
+                        if (mysqli_num_rows($getDecks) > 0) {
+                            while ($deck = mysqli_fetch_assoc($getDecks)) {
+                                $deckID = $deck["deck_id"];
+                                $name = $deck["name"];
+                                $isLeaf = $deck["is_leaf"];
 
-                                    if ($isLeaf == 0) {
-                                        echo "<div class = 'deck'>";
-                                            echo "<div class = 'labelWrapper'>";
-                                                echo "<div class = 'label'>";
-                                                    echo "<div class = 'deckTitle'>";
-                                                        echo "<span class = 'expand'>▶</span>";
-                                                        echo "<span class = 'deckName'>$name</span>";
-                                                    echo "</div>";
-                                                    if(mysqli_num_rows(mysqli_query($con, "SELECT 1 FROM junction_deck_classroom WHERE deck_id = '$deckID' AND classroom_id = '$classroom_id'")) > 0) {
-                                                        echo "<span onclick=\"removeDeck('$deckID')\" class = 'addDeck added'>+</span>";
-                                                    }
-                                                    else {
-                                                        echo "<span onclick=\"addDeck('$deckID')\" class='addDeck'>+</span>";
-                                                    }
-                                                echo "</div>";
-                                            echo "</div>";
-                                            $getChildren = mysqli_query($con, "SELECT deck_id FROM decks WHERE parent_deck_id = '$deckID' ORDER BY name ASC");
-                                            while($children = mysqli_fetch_assoc($getChildren)) {
-                                                getDecks($children["deck_id"]);
-                                            }
-                                        echo "</div>";
+                                if ($isLeaf == 0) {
+                                    echo "<div class = 'deck'>";
+                                    echo "<div class = 'labelWrapper'>";
+                                    echo "<div class = 'label'>";
+                                    echo "<div class = 'deckTitle'>";
+                                    echo "<span class = 'expand'>▶</span>";
+                                    echo "<span class = 'deckName'>$name</span>";
+                                    echo "</div>";
+                                    if (mysqli_num_rows(mysqli_query($con, "SELECT 1 FROM junction_deck_classroom WHERE deck_id = '$deckID' AND classroom_id = '$classroom_id'")) > 0) {
+                                        echo "<span onclick=\"removeDeck('$deckID')\" class = 'addDeck added'>+</span>";
                                     } else {
-                                        echo "<div class = 'deck'>";
-                                            echo "<div class = 'labelWrapper'>";
-                                                echo "<div class = 'label'>";
-                                                    echo "<div class = 'deckTitle'>";
-                                                        echo "<span style = 'opacity: 0;' class = 'expand'>▶</span>";
-                                                        echo "<span class = 'deckName'>$name</span>";
-                                                    echo "</div>";
-                                                    if(mysqli_num_rows(mysqli_query($con, "SELECT classroom_id FROM junction_deck_classroom WHERE deck_id = '$deckID' AND classroom_id = '$classroom_id'")) > 0) {
-                                                        echo "<span onclick=\"removeDeck('$deckID')\" class = 'addDeck added'>+</span>";
-                                                    }
-                                                    else {
-                                                        echo "<span onclick=\"addDeck('$deckID')\" class='addDeck'>+</span>";
-                                                    }
-                                                echo "</div>";
-                                            echo "</div>";
-                                        echo "</div>";
+                                        echo "<span onclick=\"addDeck('$deckID')\" class='addDeck'>+</span>";
                                     }
+                                    echo "</div>";
+                                    echo "</div>";
+                                    $getChildren = mysqli_query($con, "SELECT deck_id FROM decks WHERE parent_deck_id = '$deckID' ORDER BY name ASC");
+                                    while ($children = mysqli_fetch_assoc($getChildren)) {
+                                        getDecks($children["deck_id"]);
+                                    }
+                                    echo "</div>";
+                                } else {
+                                    echo "<div class = 'deck'>";
+                                    echo "<div class = 'labelWrapper'>";
+                                    echo "<div class = 'label'>";
+                                    echo "<div class = 'deckTitle'>";
+                                    echo "<span style = 'opacity: 0;' class = 'expand'>▶</span>";
+                                    echo "<span class = 'deckName'>$name</span>";
+                                    echo "</div>";
+                                    if (mysqli_num_rows(mysqli_query($con, "SELECT classroom_id FROM junction_deck_classroom WHERE deck_id = '$deckID' AND classroom_id = '$classroom_id'")) > 0) {
+                                        echo "<span onclick=\"removeDeck('$deckID')\" class = 'addDeck added'>+</span>";
+                                    } else {
+                                        echo "<span onclick=\"addDeck('$deckID')\" class='addDeck'>+</span>";
+                                    }
+                                    echo "</div>";
+                                    echo "</div>";
+                                    echo "</div>";
                                 }
                             }
                         }
-                        getDecks("root");
+                    }
+                    getDecks("root");
                     ?>
                 </div>
             </div>
@@ -222,7 +223,7 @@ $classroom_name = mysqli_fetch_array($result_classroom);
                                 Classroom Information
                             </span>
                             <span class="title" id="contain">
-                                <?php 
+                                <?php
                                 echo $classroom_name['name'];
                                 ?>
                             </span>
@@ -307,19 +308,47 @@ $classroom_name = mysqli_fetch_array($result_classroom);
                 $query = "SELECT * FROM junction_classroom_user WHERE classroom_id = '$classroom_id' AND classroom_role_id = '3'";
                 $result = mysqli_query($con, $query);
                 while ($classroom_line = mysqli_fetch_array($result)) {
+                    // Get the student ID
                     $user_id_student = $classroom_line['user_id'];
                     $query2 = "SELECT * FROM users WHERE user_id = '$user_id_student'";
                     $result2 = mysqli_query($con, $query2);
                     $line_name = mysqli_fetch_assoc($result2);
                     $temp_name = $line_name['name'];
+
+                    // Get the student RGB
+                    $result_rgb = mysqli_query($con, "
+                    SELECT * 
+                    FROM junction_deck_classroom 
+                    JOIN junction_deck_card jdcard
+                        ON junction_deck_classroom.deck_id = jdcard.deck_id
+                    JOIN card_progress cp
+                        ON cp.card_id = jdcard.card_id AND user_id = '$user_id_student'
+                    WHERE junction_deck_classroom.classroom_id = '$classroom_id';
+                    ");
+
+                    // Count RGB Manually PHP
+                    $count_rgb = mysqli_fetch_array($result_rgb);
+                    date_default_timezone_set("Asia/Jakarta");
+                    $now = new DateTime();
+                    $db_datetime = new DateTime($count_rgb['review_due']);
+                    $red = $green = $grey = 0;
+                    for ($i = 0; $i < mysqli_num_rows($result_rgb); $i++) {
+                        if ($db_datetime <= $now) {
+                            $red++;
+                        } else {
+                            $green++;
+                        }
+                        $grey++;
+                    }
+
                     echo " <div class='title-student' onclick='ClickToDP(this)' data-id='$user_id_student'>
                     <!-- Deck Title -->
                     <span class='title'>$temp_name</span>
                     <!-- To Review Green Red Blue-->
                     <div class='to-review'>
-                        <span class='green'>169</span>
-                        <span class='red'>28</span>
-                        <span class='blue'>1638</span>
+                        <span class='red'>$red</span>
+                        <span class='green'>$green</span>
+                        <span class='blue'>$grey</span>
                     </div>
                 </div>";
                 }
