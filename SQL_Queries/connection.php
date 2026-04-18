f8e3825324038d22d21b2f729ba0fd58f18ffe55
Leonard Samuel Setiawan <leonsamuels080306@gmail.com>
2026-04-18 23:03:54 +0700

    fix: handled empty decks in card swipe, style: added 'Study Again' or 'Remember' on the bottom of the card when swiping, feature: added language switch in card swipe tutorial

--- SQL_Queries/connection.php
+++ SQL_Queries/connection.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect("localhost", "anki_marvel", "ihatep0tat0", "anki");
-// $con = mysqli_connect("localhost", "root", "", "anki");
+// $con = mysqli_connect("localhost", "anki_marvel", "ihatep0tat0", "anki");
+$con = mysqli_connect("localhost", "root", "", "anki");
