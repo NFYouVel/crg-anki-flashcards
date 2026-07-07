208252c1c82b33c836debd2a2b8ff0fcc30afcde
Leon080306 <leonsamuels080306@gmail.com>
2026-07-07 21:31:35 +0700

    fixed con

--- SQL_Queries/connection.php
+++ SQL_Queries/connection.php
@@ -1,8 +1,3 @@
 <?php
-<<<<<<< Updated upstream
 $con = mysqli_connect("localhost", "anki_marvel", "maNwnLDchGgkpJRrEfPYUOaM2a5vhl66xhEz3QGR4R0BDF4FEB", "anki");
 // $con = mysqli_connect("localhost", "root", "", "anki");
-=======
-// $con = mysqli_connect("localhost", "anki_marvel", "ihatep0tat0", "anki");
-$con = mysqli_connect("localhost", "root", "", "anki");
->>>>>>> Stashed changes
