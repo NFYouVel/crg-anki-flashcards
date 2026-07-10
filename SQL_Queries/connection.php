852154cf9eae051ce8cf7889bf4aefa9aefc4caf
Leon080306 <leonsamuels080306@gmail.com>
2026-07-10 11:57:22 +0700

    fixed con

--- SQL_Queries/connection.php
+++ SQL_Queries/connection.php
@@ -1,3 +1,3 @@
 <?php
-// $con = mysqli_connect("localhost", "anki_marvel", "maNwnLDchGgkpJRrEfPYUOaM2a5vhl66xhEz3QGR4R0BDF4FEB", "anki");
-$con = mysqli_connect("localhost", "root", "", "anki");
+$con = mysqli_connect("localhost", "anki_marvel", "maNwnLDchGgkpJRrEfPYUOaM2a5vhl66xhEz3QGR4R0BDF4FEB", "anki");
+// $con = mysqli_connect("localhost", "root", "", "anki");
